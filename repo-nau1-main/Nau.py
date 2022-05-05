e2424c540df51276d16e6a56b76c52d22285332f
AdriGuti <97900094+AdriGuti@users.noreply.github.com>
2022-05-05 19:06:44 +0200

    Add files via upload

---
+++ repo-nau1-main/Nau.py
@@ -0,0 +1,22 @@
+class Nau:
+
+    def __init__(self,x,y):        
+        self.x=x
+        self.y=y
+        self.incr=1
+    
+    def moure_dreta(self):
+        self.x=self.x+self.incr
+        
+    def moure_esquerre(self):
+        self.x=self.x-self.incr
+
+    def moure_amunt(self):
+        self.y=self.y+self.incr
+        
+    def moure_avall(self):
+        self.y=self.y-self.incr
+        
+    def mostrar_nau(self):
+        print(f"[{self.x},{self.y}]")
+
